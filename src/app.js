eaadaa156f68314de85c7b532fa8dd7ad2cf89b1
valdimir-makarov <2010049@student.ruet.ac.bd>
2024-01-06 03:35:15 +0600

    resgiter data uploading to db and file avatar uploading in cloudinary and db

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(cors({
 
 
 app.use(express.json({limit:"16kb"}))//this will monitor how much request can come at a time
-app.use(express.urlencoded({extended:true,limit:"13kb"}))
+app.use(express.urlencoded({extended:true,limit:"100kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
